9b8512f743a2574f2fcc56df7ba67f88760a73b1
Dillion Wang <dillion@Dillions-MacBook-Pro.local>
2018-12-16 13:25:28 +0800

    add util to get page index from request

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -4,7 +4,6 @@ import hashlib
 import requests
 from bs4 import BeautifulSoup
 
-
 def check_fdu_auth(username, password):
 
     form_data = {
@@ -33,6 +32,19 @@ def get_md5(bytes):
     hash = hashlib.md5()
     hash.update(bytes)
     return hash.hexdigest()
+
+def get_index_st_en(request, PG_SIZE=20):
+
+    try:
+        page = int(request.GET['page'])
+    except:
+        page = 0
+
+    index_st = PG_SIZE * page
+    index_en = PG_SIZE * (page + 1)
+
+    return index_st, index_en
+
 
 if __name__ == "__main__":
     print(check_fdu_auth("17210240047", "hello"))
