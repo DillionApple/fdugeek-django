468a07840855a4ef68a3ebf12a168966a4cd80c4
Dillion Wang <dillion@Dillions-MacBook-Pro.local>
2019-02-12 11:57:34 +0800

    minor fix. add feedback migration info

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from feedback.models import Feedback
+
+admin.site.register(Feedback)
--- feedback/urls.py
+++ feedback/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from feedback import views
 
 urlpatterns = [
+    path('', views.feedback),
     path('feedback/', views.feedback),
 ]
